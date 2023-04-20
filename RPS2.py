80f3c579a2e2415142efcee01895a14dca9d96d9
Volpato <gvolpatolima@gmail.com>
2023-04-19 23:32:33 -0300

    Still trying

--- RPS2.py
+++ RPS2.py
@@ -1,23 +1,76 @@
+## Done for god's sake
+
+import itertools
 import random
 
-def player(p1_prev_play, opponent_history=[], n=2, corpus=["rock", "paper", "scissors"], markov_chain={}):
-    # Check if p1_prev_play is in opponent_history
-    if p1_prev_play in opponent_history:
-        sub_order = {}
-        for i in range(len(opponent_history)):
-            if opponent_history[i] == p1_prev_play and i + n < len(opponent_history):
-                suffix = opponent_history[i + n]
-                if suffix in sub_order:
-                    sub_order[suffix] += 1
-                else:
-                    sub_order[suffix] = 1
 
-        # Check if sub_order is not empty
-        if sub_order:
-            prediction = max(sub_order, key=sub_order.get)[-1:]
-        else:
-            prediction = random.choice(corpus)  # Choose randomly if sub_order is empty
+def create_matrix(order):
+    moves = ['R', 'P', 'S']
+    for i in range((order * 2 - 1)):
+        moves += [''.join(i) for i in itertools.product(moves, ''.join(moves))]
+    matrix = {key: {'R': {'prob': 1/3, 'n_obs': 0},
+                    'P': {'prob': 1/3, 'n_obs': 0},
+                    'S': {'prob': 1/3, 'n_obs': 0}} for key in moves}
+    return matrix
+
+print(create_matrix(1))
+
+
+def update_matrix(matrix, pair, input, decay):
+    for i in matrix[pair]:
+        matrix[pair][i]['n_obs'] *= decay
+
+    matrix[pair][input]['n_obs'] += 1
+
+    n_total = sum(matrix[pair][i]['n_obs'] for i in matrix[pair])
+
+    for i in matrix[pair]:
+        matrix[pair][i]['prob'] = matrix[pair][i]['n_obs'] / n_total
+
+def predict(matrix, pair):
+    probs = matrix[pair]
+
+    y = [probs[k]['prob'] for k in probs]
+    x = list(probs.keys())
+
+    if max(y) == min(y):
+        return random.choice(['R', 'P', 'S'])
     else:
-        prediction = random.choice(corpus)  # Choose randomly if p1_prev_play is not in opponent_history
+        max_index = y.index(max(y))
+        return x[max_index]
 
-    return prediction
+import random
+
+beat = {'R': 'P', 'P': 'S', 'S': 'R'}
+
+m = create_matrix(1)
+output = ""
+pair_diff1 = ""
+pair_diff2 = ""
+
+def player(prev_play):
+    global pair_diff1, pair_diff2, output, m
+    if not prev_play:
+        reset()
+
+    input = prev_play
+
+    pair_diff2 = pair_diff1
+    pair_diff1 = output + input
+
+
+    if pair_diff2 != '':
+        update_matrix(m, pair_diff2, input, 0.9)
+        output = beat[predict(m, pair_diff1)]
+    else:
+        output = random.choice(['R', 'P', 'S'])
+
+    return output
+
+
+def reset():
+    global m, output, pair_diff1, pair_diff2
+    m = create_matrix(1)
+    pair_diff1 = ""
+    pair_diff2 = ""
+    output = ""
--- test.py
+++ test.py
@@ -1,47 +1,74 @@
+import itertools
 import random
 
 
-def generate_markov_chain(corpus, n=1):
-    markov_chain = {}
+def create_matrix(order):
+    moves = ['R', 'P', 'S']
+    for i in range((order * 2 - 1)):
+        moves += [''.join(i) for i in itertools.product(moves, ''.join(moves))]
+    matrix = {key: {'R': {'prob': 1/3, 'n_obs': 0},
+                    'P': {'prob': 1/3, 'n_obs': 0},
+                    'S': {'prob': 1/3, 'n_obs': 0}} for key in moves}
+    return matrix
 
-    # Loop through the corpus to generate the Markov chain
-    for i in range(len(corpus)):
-        if i + n < len(corpus):
-            prefix = tuple(corpus[i:i + n])  # Use a tuple as the key for the prefix
-            suffix = corpus[i + n]  # Use the next word as the suffix
-
-            if prefix in markov_chain:
-                markov_chain[prefix].append(suffix)
-            else:
-                markov_chain[prefix] = [suffix]
-
-    return markov_chain
+print(create_matrix(1))
 
 
-def get_opponent_play(prev_play, opponent_history):
-    opponent_history.append(prev_play)  # Add the previous play to the history
-    prefix = tuple(opponent_history[-n:])  # Use the last n plays as the prefix
+def update_matrix(matrix, pair, input, decay):
+    for i in matrix[pair]:
+        matrix[pair][i]['n_obs'] *= decay
 
-    if prefix in markov_chain:
-        suffixes = markov_chain[prefix]
-        return random.choice(suffixes)
+    matrix[pair][input]['n_obs'] += 1
+
+    n_total = sum(matrix[pair][i]['n_obs'] for i in matrix[pair])
+
+    for i in matrix[pair]:
+        matrix[pair][i]['prob'] = matrix[pair][i]['n_obs'] / n_total
+
+def predict(matrix, pair):
+    probs = matrix[pair]
+
+    y = [probs[k]['prob'] for k in probs]
+    x = list(probs.keys())
+
+    if max(y) == min(y):
+        return random.choice(['R', 'P', 'S'])
     else:
-        return random.choice(["rock", "paper", "scissors"])  # If prefix not found, choose randomly
+        max_index = y.index(max(y))
+        return x[max_index]
+
+import random
+
+beat = {'R': 'P', 'P': 'S', 'S': 'R'}
+
+m = create_matrix(1)
+output = ""
+pair_diff1 = ""
+pair_diff2 = ""
+
+def player(prev_play):
+    global pair_diff1, pair_diff2, output, m
+    if not prev_play:
+        reset()
+
+    input = prev_play
+
+    pair_diff2 = pair_diff1
+    pair_diff1 = output + input
 
 
-# Global variables
-corpus = ["rock", "paper", "scissors"]  # Corpus of possible plays
-n = 2  # Set the order of the Markov chain (prefix length)
-markov_chain = generate_markov_chain(corpus, n)  # Generate the Markov chain
+    if pair_diff2 != '':
+        update_matrix(m, pair_diff2, input, 0.9)
+        output = beat[predict(m, pair_diff1)]
+    else:
+        output = random.choice(['R', 'P', 'S'])
+
+    return output
 
 
-def player(prev_play, opponent_history=[]):
-    opponent_play = get_opponent_play(prev_play, opponent_history)
-    return opponent_play
-
-
-# Example usage
-prev_play = "rock"
-opponent_history = ["scissors", "paper", "rock"]
-next_play = player(prev_play, opponent_history)
-print("Next play:", next_play)
+def reset():
+    global m, output, pair_diff1, pair_diff2
+    m = create_matrix(1)
+    pair_diff1 = ""
+    pair_diff2 = ""
+    output = ""
