4b866a6ec0910eaff96b716f8941ecb8735c73ab
Volpato <gvolpatolima@gmail.com>
2023-04-19 21:42:49 -0300

    test

---
+++ test.py
@@ -0,0 +1,53 @@
+def abbey(prev_opponent_play,
+          opponent_history=[],
+          play_order=[{
+              "RR": 0,
+              "RP": 0,
+              "RS": 0,
+              "PR": 0,
+              "PP": 0,
+              "PS": 0,
+              "SR": 0,
+              "SP": 0,
+              "SS": 0,
+          }]):
+
+    if not prev_opponent_play:
+        prev_opponent_play = 'R'
+    opponent_history.append(prev_opponent_play)
+
+    last_two = "".join(opponent_history[-2:])
+    if len(last_two) == 2:
+        play_order[0][last_two] += 1
+
+    potential_plays = [
+        prev_opponent_play + "R",
+        prev_opponent_play + "P",
+        prev_opponent_play + "S",
+    ]
+
+    sub_order = {
+        k: play_order[0][k]
+        for k in potential_plays if k in play_order[0]
+    }
+
+    prediction = max(sub_order, key=sub_order.get)[-1:]
+
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    return ideal_response[prediction]
+
+import random
+
+# List of possible choices
+choices = ["R", "P", "S"]
+
+# Generate an array of length 20 with random choices
+random_array = [random.choice(choices) for _ in range(20)]
+
+print(random_array)
+
+# Iterate over the array and call the function with each element as input
+for choice in random_array:
+    print(abbey(choice))
+
+
